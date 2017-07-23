dd452a2bd9a4f7c6f42a3bfbddde37798fe01eaf
Avital Ben Natan <avital.bennatan@gmail.com>
2017-07-23 22:25:37 +0300

    update libjsonnet and attempt at revival

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ var libjsonnet = require('./lib/libjsonnet');
 var Jsonnet = function() {
   var jsonnet_make = libjsonnet.cwrap('jsonnet_make', 'number', []);
   this.vm = jsonnet_make();
-  this.jsonnet_cleanup_string = libjsonnet.cwrap('jsonnet_cleanup_string', 'number', ['number', 'number']);
+  this.jsonnet_realloc = libjsonnet.cwrap('jsonnet_realloc', 'number', ['number', 'number', 'number'])
+  //this.jsonnet_cleanup_string = libjsonnet.cwrap('jsonnet_cleanup_string', 'number', ['number', 'number']);
   this.jsonnet_evaluate_snippet = libjsonnet.cwrap('jsonnet_evaluate_snippet', 'number', ['number', 'string', 'string', 'number']);
   this.jsonnet_destroy = libjsonnet.cwrap('jsonnet_destroy', 'number', ['number']);
 };
@@ -17,7 +18,9 @@ Jsonnet.prototype.eval = function(code) {
   var error = libjsonnet.getValue(error_ptr, 'i32*');
   libjsonnet._free(error_ptr);
   var result = libjsonnet.Pointer_stringify(output_ptr);
-  this.jsonnet_cleanup_string(this.vm, output_ptr);
+  //this.jsonnet_cleanup_string(this.vm, output_ptr);
+  this.jsonnet_realloc(vm, output_ptr, 0);
+  this.jsonnet_destroy(vm)
   if (error) {
     throw new Error(result);
   }
